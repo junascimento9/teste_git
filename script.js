ccaf6707f5750062cd330e90a2b724b998d10201
Juliana Nascimento <91079930+junascimento9@users.noreply.github.com>
2023-03-31 00:57:41 +0000

    JS

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
-//manipulação de DOM
-//const nav = document.querySelector('nav');
-//const produtos = document.querySelector('.produtos a');
-//console.log(nav)
-//console.log(produtos)
-//console.log(produtos.href)
-//produtos.remove()
-//nav.style.backgroundColor="black"
-//nav.style.padding="1rem"
+const botao = document.querySelector('button');
 
-//contas simples
-//let total = 0;
-//const compras = 100;
-//const imposto = 10;
-//total = compras + imposto;
-//const dobro = total * 2; //multiplicação
-//console.log(dobro);
-//const metade = total / 2; //divisão
-//const desconto = total - 20;
-//const strings = '20' + '20'; // 2020
-//const numbers = 20 + 20; // 40
-
-//const nome = 'O senhor dos Anéis';
-//const ano = 1954;
-//const autor = 'J. R R. Tolkien';
-
-//const nome = 'O senhor dos Anéis'
-
-const circulo = document.querySelector("#circulo");
-
-function seguirMouse(event) {
-  circulo.style.top = event.y + "px";
-  circulo.style.top = event.x + "px";
+function somar () {
+  const div = document.querySelector('div');
+  const total = Number(div.innerText) + 1;
+  if(total < 10){
+    div.innerText = total;
+  }
+  else {
+    console.log('impossivel realizar operacion')
+  }
 }
 
-window.addEventListener("mousemove", seguirMouse);
+if (botao) {
+  botao.addEventListener('click', somar);
+} 
+
+console.log(botao)
