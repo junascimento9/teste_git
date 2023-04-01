92a4cb979fc48138643da091231e00354b3ebeca
Juliana Nascimento <91079930+junascimento9@users.noreply.github.com>
2023-04-01 02:11:25 +0000

    TesteJS

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
-const botao = document.querySelector('button');
+//const botao = document.querySelector('button');
 
-function somar () {
-  const div = document.querySelector('div');
-  const total = Number(div.innerText) + 1;
-  if(total < 10){
-    div.innerText = total;
-  }
-  else {
-    console.log('impossivel realizar operacion')
-  }
+//function somar () {
+  //const div = document.querySelector('div');
+  //const total = Number(div.innerText) + 1;
+  //if(total < 10){
+    //div.innerText = total;
+  //}
+  //else {
+    //console.log('impossivel realizar operacion')
+  //}
+//}
+
+//if (botao) {
+  //botao.addEventListener('click', somar);
+//} 
+
+//console.log(botao)
+
+
+//const links = document.querySelectorAll('nav a');
+
+
+//function ativarLink(link){
+    //const href = link.href;
+    //const url = document.location.href;
+
+    //if(href === url){
+        //link.style.backgroundColor = 'black';
+        //link.style.color = 'white';
+    //}
+    //console.log(url) 
+    //console.log(href)
+//}
+
+//links.forEach(ativarLink)
+
+
+//SOLUÇÃO DE PROBLEMAS//
+
+const imagens = document.querySelectorAll('#galeria li img');
+
+function galeriaTrocar(event) {
+  const principal = document.querySelector('#imagem-principal')
+  const clicada = event.currentTarget;
+  principal.src = clicada.src;
+  principal.alt = clicada.alt;
+  //opção 2//
+  //principal.setAttribute('alt', clicada.getAttribute('alt'))
+  console.log(clicada.src);
 }
 
-if (botao) {
-  botao.addEventListener('click', somar);
-} 
+function galeriaClique(imagem) {
+  imagem.addEventListener('click', galeriaTrocar);
+}
 
-console.log(botao)
+imagens.forEach(galeriaClique);
